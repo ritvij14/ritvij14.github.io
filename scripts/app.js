3dcf15fb4bfdc214be0fb688c2ce2ce47b7deabe
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2020-08-11 03:45:45 +0530

    Added nav-button selectors

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+var homeBtn = document.getElementById("home-nav");
+var aboutBtn = document.getElementById("about-nav");
+var expBtn = document.getElementById("exp-nav");
+var contactBtn = document.getElementById("contact-nav");
+
+var navButtons = [homeBtn, aboutBtn, expBtn, contactBtn];
+
+navButtons.forEach((btn) => {
+    btn.addEventListener("click", () => { navClick(btn) });
+});
+
+function navClick(btn) {
+    if (!(btn.classList.contains("active"))) {
+        var current = document.getElementsByClassName("active");
+        current[0].classList.remove("active");
+        btn.classList.add("active");
+    }
+}
